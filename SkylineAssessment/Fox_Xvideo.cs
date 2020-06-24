b6daa14631d53f518e385fa8c6e223e45cc09605
Sumathi Rajendran <sumathirajendran70@gmail.com>
2020-06-24 21:39:58 +0800

    Commit

---
+++ SkylineAssessment/Fox_Xvideo.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkylineAssessment
+{
+    public class Fox_Xvideo
+    {
+        [JsonProperty("Device")]
+        public string device { get; set; }
+        [JsonProperty("Model")]
+        public string model { get; set; }
+        [JsonProperty("NIC")]
+        public List<NIC> NICs { get; set; }
+    }
+
+    public class NIC {
+        [JsonProperty("Description")] 
+        public string description { get; set; }
+        [JsonProperty("MAC")] 
+        public string MAC { get; set; }
+        [JsonProperty("Timestamp")] 
+        public string ts { get; set; }
+        [JsonProperty("Rx")] 
+        public string rx { get; set; }
+        [JsonProperty("Tx")] 
+        public string tx { get; set; }
+    }
+
+    public class output {
+        [JsonProperty("Description")]
+        public string description { get; set; }
+        [JsonProperty("MAC")]
+        public string MAC { get; set; }
+        [JsonProperty("Timestamp")]
+        public string ts { get; set; }
+
+        [JsonProperty("bitrateRx")]
+        public long bitrateRx { get; set; }
+        [JsonProperty("bitrateTx")]
+        public long bitrateTx { get; set; }
+    }
+}
+
+
---
+++ SkylineAssessment/Program.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkylineAssessment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string API_Output = @"{'Device':'Arista','Model':'X-Video','NIC':[{'Description':'Linksys ABR','MAC':'14:91:82:3C:D6:7D','Timestamp': '2020-03-23T18:25:43.511Z','Rx': '3698574500','Tx': '122558800'}] }";
+                int frequency = 2;
+                List<output> outputList = new List<output>();
+                output outObj = new output();
+                //Formula used to calculate bitrate is "Octets * frequency" for reception and transmission
+                var parsed = JsonConvert.DeserializeObject<Fox_Xvideo>(API_Output);
+                for (int i = 0; i < parsed.NICs.Count; i++)
+                {
+                    outObj.description = parsed.NICs[i].description;
+                    outObj.MAC = parsed.NICs[i].MAC;
+                    outObj.ts = parsed.NICs[i].ts;
+                    outObj.bitrateRx = Int64.Parse(parsed.NICs[i].rx) * frequency;
+                    outObj.bitrateTx = Int64.Parse(parsed.NICs[i].tx) * frequency;
+                    outputList.Add(outObj);
+                }
+
+                string result = JsonConvert.SerializeObject(outputList);
+                Console.WriteLine(result);
+                Console.Read();
+            }
+            catch(Exception e)
+            {
+                Console.Write("Error occured :" , e.Message);
+            }
+        }
+    }
+}
